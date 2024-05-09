42bf6d32f9524e32afe35509060528541d3fa92a
Henry <alejandro.jayaa@gmail.com>
2024-05-09 16:08:21 -0500

    Clase

--- JavaApplication20/src/Terreno.java
+++ JavaApplication20/src/Terreno.java
@@ -1,7 +1,21 @@
 public class Terreno{
-    public double alto; 
-    public double ancho;
-    public double vmc;
-    public double area;
-    public double costoFinal;
+    public double alto, ancho, vmc, area, costoFinal;
+    public Terreno(double alto, double ancho, double vmc) {
+        this.alto = alto;
+        this.ancho = ancho;
+        this.vmc = vmc;
+    }
+    public void calcularArea(){
+        this.area = this.alto*this.ancho;}
+    public void calcularCostoFinal(){
+        costoFinal = area*vmc;
+        if (area > 300)
+            costoFinal *= 0.8; //costoFinal =  (area*vmc) - ( area*vmc)*0.2;
+        else if (area >= 200)
+                costoFinal *= 0.9; //costoFinal =  (area*vmc) - (area*vmc)*0.1;
+    }
+    @Override
+    public String toString() {
+        return "Terreno{" + "alto=" + alto + ", ancho=" + ancho + ", vmc=" + vmc + ", area=" + area + ", costoFinal=" + costoFinal + '}';
+    }
 }
